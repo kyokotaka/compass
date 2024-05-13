c7da91c1daf925ee99f62c0fa3faa9260875d4fd
kyokotaka <162997773+kyokotaka@users.noreply.github.com>
2024-05-13 22:40:10 +0900

    Add files via upload

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\Models\Users\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use DB;
+
+use App\Models\Users\Subjects;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    public function registerView()
+    {
+        $subjects = Subjects::all();
+        return view('auth.register.register', compact('subjects'));
+    }
+
+    public function registerPost(Request $request)
+    {$validated =Validator::make ($request->all(),[
+        'over_name'=>['required,string,max:10'],
+        'under_name'=>['required,string,max:10'],
+        'over_name_kana'=>['required,string,/\A[ァ-ヶー]+\z/,max:30'],
+        'under_name_kana'=>['required,string,/\A[ァ-ヶー]+\z/,max:30'],
+        'mail_address'=>['required,email,unique:users,max:100'],
+        'sex'=>['required,in:1,2,3'],
+        'old_year'=>['required,date_format:"Y"'],
+        'old_month'=>['required,between1,12'],
+        'old_month'=>['required,between1,31'],
+        'role'=>['required,in:1,2,3,4'],
+        'password'=>['required,min:8,max:30,confirmed'],
+        // 'password_confirmation'=>['required,min:8,max:30,']
+    ]);
+    dd($validated);
+        DB::beginTransaction();
+        try{
+            $old_year = $request->old_year;
+            $old_month = $request->old_month;
+            $old_day = $request->old_day;
+            $data = $old_year . '-' . $old_month . '-' . $old_day;
+            $birth_day = date('Y-m-d', strtotime($data));
+            $subjects = $request->subject;
+
+            $user_get = User::create([
+                'over_name' => $request->over_name,
+                'under_name' => $request->under_name,
+                'over_name_kana' => $request->over_name_kana,
+                'under_name_kana' => $request->under_name_kana,
+                'mail_address' => $request->mail_address,
+                'sex' => $request->sex,
+                'birth_day' => $birth_day,
+                'role' => $request->role,
+                'password' => bcrypt($request->password)
+            ]);
+            $user = User::findOrFail($user_get->id);
+            $user->subjects()->attach($subjects);
+            DB::commit();
+            return view('auth.login.login');
+        }catch(\Exception $e){
+            DB::rollback();
+            return redirect()->route('loginView');
+        }
+    }
+}
